20dc3456320a5b838dfc8191e5538e51c05f7d2a
shhith <53967112+shhith@users.noreply.github.com>
2021-08-23 22:10:29 +0900

    arrow function

--- src/index.js
+++ src/index.js
@@ -1,10 +1,24 @@
-import "./styles.css";
+// アロー関数
 
-document.getElementById("app").innerHTML = `
-<h1>Hello Vanilla!</h1>
-<div>
-  We use the same configuration as Parcel to bundle this sandbox, you can find more
-  info about Parcel 
-  <a href="https://parceljs.org" target="_blank" rel="noopener noreferrer">here</a>.
-</div>
-`;
+// 従来の関数
+function func1(str) {
+  return str;
+}
+const func2 = function (str) {
+  return str;
+};
+console.log(func1("func1です！"));
+console.log(func2("にゃんにゃん"));
+
+// アロー関数...=>が矢印のようなのでアロー関数と呼ばれている
+// 引数を書いて、=>を書いて、その後に{}で処理を書いていく
+const func3 = (str) => {
+  return str;
+};
+console.log(func3("アロー関数っです"));
+
+// 引数が一つの場合、()を省略できる
+// 中の式が一行で終わってそれを返却するような単一式の場合、returnを省略できる
+
+const func4 = (num1, num2) => num1 + num2;
+console.log(func4(10, 20));
